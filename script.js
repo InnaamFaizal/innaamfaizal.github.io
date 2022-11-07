3bfcf1c9a12e0af88903588f281811a8812645e3
Innaam Faizal <innaamfaizal@gmail.com>
2022-11-07 12:52:12 +0530

    Commit 3
    Final Commit

--- script.js
+++ script.js
@@ -118,3 +118,8 @@ function draw(){
     drawBlob(x,y,vx,vy,ax,ay);
 
 } // end draw()
+window.addEventListener("keydown", function(e) {
+    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
